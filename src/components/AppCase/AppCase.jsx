991a1a5f340089499bb20068eec425e305c859e8
NIKITA <s0172058@edu.kubsu.ru>
2023-01-22 21:50:24 +0300

    1111111

---
+++ src/components/AppCase/AppCase.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import "./AppCase.css"
+
+const AppExp = () => {
+	return (
+		<div className="w-[100vw] bg-exp h-[840px] relative -z-10 exp">
+			<div className="container flex justify-end items-center">
+				<div className="w-1/2">
+					<h2 className="font-bold text-5xl text-white mb-16 mt-64 leading-[54px]">Экспертиза в Drupal,<br/> опыт 14 лет!</h2>
+					<div className="flex justify-between items-center gap-4">
+						<p className="pl-6 py-4 border-l-4 text-white opacity-70 text-xl border-primary">Только системный подход – контроль версий, резервирование
+							и тестирование!</p>
+						<p className="pl-6 py-4 border-l-4 text-white opacity-70 text-xl border-primary">Только Drupal сайты,
+							не берем на поддержку сайты на других CMS!</p>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default AppCase;
