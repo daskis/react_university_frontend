fd124aee89b6615cb9c56509448f26b7f7abe634
NIKITA <s0172058@edu.kubsu.ru>
2023-01-24 12:26:30 +0300

    asd

--- src/components/AppFooter/AppFooter.jsx
+++ src/components/AppFooter/AppFooter.jsx
@@ -20,6 +20,8 @@ const AppFooter = () => {
 						<p className="flex items-center mt-4 text-base font-semibold underline"><img className="pr-4" src={mail} alt=""/>info@drupal-coder.ru</p>	
 					
 					</div>
+					
+
 					<div className="pt-20 w-[40%] flex flex-col p-2 text-base gap-y-2">
 						<input type="text" className="py-5 bg-inherit border rounded" placeholder="     Ваше имя"/>
 						<input type="text" className="py-5 bg-inherit border rounded" placeholder="     Телефон"/>
@@ -28,14 +30,10 @@ const AppFooter = () => {
 						
 						<p className="pt-2 text-white">Отправляя заявку, я даю согласие на обработку своих персональных данных</p>
 						<button className="text-white bg-[#F14D34] py-4 px-10 rounded-md">Оставить заявку!</button>
-
-						<div className="text-white text-left">
-							<p className="text-xs font-light">Проект ООО «Инитлаб», Краснодар, Россия.</p>
-							<p className="pt-2 text-xs font-light">Drupal является зарегистрированной торговой маркой Dries Buytaert.</p>
-
-						</div>
 					</div>
 
+					
+					
 
 				</div>
 			</div>
