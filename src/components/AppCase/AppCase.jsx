5a99bf496c6c0a0ad31ede708f19b823de0868c0
NIKITA <s0172058@edu.kubsu.ru>
2023-01-24 12:26:54 +0300

    asd

--- src/components/AppCase/AppCase.jsx
+++ src/components/AppCase/AppCase.jsx
@@ -8,12 +8,12 @@ import case6 from "../../assets/case6.jpg"
 import case7 from "../../assets/case7.png"
 
 const AppCase = () => {
+
 	return (
 		<div className="Case">
 			<div className="container">
 				<h2 className="text-center text-secondary font-bold text-4xl mt-36 mb-20">Последние кейсы</h2>
 				
-
 				<div class="grid grid-cols-3 gap-4">
 					<div className=""><img src={case1} alt=""/>
 						<p className="pl-6 mt-8 text-xl">Настройка выгрузки YML для Яндекс.Маркета</p>
@@ -45,25 +45,21 @@ const AppCase = () => {
 						<p className="mt-6 pl-6 text-xs">Эти слова совершенно справедливы, однако гипнотический рифф продолжает паузный пласт</p>
 					</div>
 
-
 					
 					<div className="col-span-2 flex items-end relative h-[474px]"><img className="absolute" src={case6} alt=""/>
 					
 					<p className="pl-6 pb-10 font-semibold text-white text-base items-end z-20">Настройка выгрузки YML для Яндекс.Маркета</p>
 					
 					</div>
-					
-					
+									
 					
 					<div className="flex flex-col items-start justify-end relative h-[474px]"><img className="absolute" src={case7} alt=""/>					
 						<p className="pl-6 mt-16 font-semibold text-white text-base items-end z-20">Настройка выгрузки YML для Яндекс.Маркета</p>
 						<p className="pl-6 pb-10 text-white text-xs items-end z-20">22.04.2019</p>
 					</div>
 
-
 				</div>
 				
-
 			</div>
 			<div className="pt-4 flex justify-center">
 				<button className="py-4 px-10 border-2 rounded-md">ПОКАЗАТЬ ЕЩЕ</button>
